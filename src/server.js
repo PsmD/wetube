c1843c0414745806c4933e386036f2ef9c0f5e52
PsmD <ehrhsdl331019@gmail.com>
2021-10-28 15:51:01 +0900

    /, /login에 request, response 작성

--- src/server.js
+++ src/server.js
@@ -4,6 +4,17 @@ const PORT = 4000;
 
 const app = express();
 
-const handleListening = () => console.log('Server listening on port http://localhost:${PORT}');
+const handlehome = (req, res) => {
+    return res.send("<h1>Hello</h2>");
+};
+const handlelogin = (req, res) => {
+    return res.send({ message: "Login here."});
+};
+
+app.get("/", handlehome);
+app.get("/login", handlelogin);
+
+const handleListening = () =>
+ console.log('Server listening on port http://localhost:${PORT}');
 
 app.listen(PORT, handleListening);
