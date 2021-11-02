ee3550cf0b9ffdf87679261abbc5b8e8886a786b
PsmD <ehrhsdl331019@gmail.com>
2021-11-02 22:39:59 +0900

    Text 수정

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
+const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
@@ -11,12 +12,12 @@ const logger = morgan("dev");
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
+app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-
 const handleListening = () =>
- console.log('Server listening on port http://localhost:4000');
+  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 
-app.listen(handleListening);
+app.listen(PORT, handleListening);
