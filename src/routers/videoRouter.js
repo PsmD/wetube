cee8375a29bc1f30b83d29715fb71adbc1b47f61
PsmD <tbvjeld7@naver.com>
2021-11-06 21:52:04 +0900

    upload code 추가

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,16 +1,17 @@
 import express from "express";
 import {
   watch,
-  upload,
-  deleteVideo,
+  getUpload,
   getEdit,
   postEdit,
+  postUpload,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
 
